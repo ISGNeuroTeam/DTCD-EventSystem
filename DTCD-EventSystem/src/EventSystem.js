7db22db12f1824abb354d45d6fbf753982f819a3
romankuramshin <rkuramshin@isgneuro.com>
2021-04-06 13:22:36 +0300

    log system integration v1

--- DTCD-EventSystem/src/EventSystem.js
+++ DTCD-EventSystem/src/EventSystem.js
@@ -1,132 +1,202 @@
-import {CustomEvent} from './utils/CustomEvent';
-import {CustomAction} from './utils/CustomAction';
-import {SystemPlugin, LogSystemAdapter} from './../../DTCD-SDK/index';
+import { CustomEvent } from './utils/CustomEvent';
+import { CustomAction } from './utils/CustomAction';
+import { SystemPlugin, LogSystemAdapter } from './../../DTCD-SDK/index';
 
 export class EventSystem extends SystemPlugin {
-	static getRegistrationMeta() {
-		return {
-			type: 'core',
-			title: 'Система cобытий',
-			name: 'EventSystem',
-			withDependencies: true,
-			priority: 6,
-			version: '0.2.0',
-		};
-	}
+  static getRegistrationMeta() {
+    return {
+      type: 'core',
+      title: 'Система cобытий',
+      name: 'EventSystem',
+      withDependencies: true,
+      priority: 6,
+      version: '0.2.0',
+    };
+  }
 
-	constructor(guid) {
-		super();
-		// systemGUID needed for getting callback (function) of instances by guid
-		this.guid = guid;
-		this.logSystem = new LogSystemAdapter(this.guid, 'EventSystem');
-		this.actions = [];
-		this.events = [];
-	}
+  constructor(guid) {
+    super();
+    // systemGUID needed for getting callback (function) of instances by guid
+    this.guid = guid;
+    this.logSystem = new LogSystemAdapter(this.guid, 'EventSystem');
+    this.actions = [];
+    this.events = [];
+  }
 
-	registerEvent(customEvent) {
-		if (customEvent instanceof CustomEvent) {
-			this.events.push(customEvent);
-			return true;
-		} else return false;
-	}
+  registerEvent(customEvent) {
+    this.logSystem.debug(`Trying to register event '${customEvent.id}`);
+    if (customEvent instanceof CustomEvent) {
+      this.events.push(customEvent);
+      this.logSystem.info(`Registered event '${customEvent.id}'`);
+      return true;
+    } else {
+      this.logSystem.debug(`Given event isn't istance of CustomEvent class`);
+      return false;
+    }
+  }
 
-	registerAction(action) {
-		if (action instanceof CustomAction) {
-			this.actions.push(action);
-			return true;
-		} else return false;
-	}
+  registerAction(action) {
+    if (action instanceof CustomAction) {
+      this.actions.push(action);
+      this.logSystem.info(`Registered action '${action.id}'`);
+      return true;
+    } else return false;
+  }
 
-	// Events methods
-	createAndPublish(guid, eventName, args) {
-		const customEvent = this.createEvent(guid, eventName, args);
-		this.publishEvent(customEvent);
-	}
+  // Events methods
+  createAndPublish(guid, eventName, args) {
+    const customEvent = this.createEvent(guid, eventName, args);
+    this.logSystem.info(`Created event '${customEvent.id}'`);
+    this.publishEvent(customEvent);
+  }
 
-	publishEvent(customEvent) {
-		if (customEvent instanceof CustomEvent) {
-			this.logSystem.info(`Publish event ${customEvent.id}`);
-			PubSub.publish(customEvent, customEvent.id);
-			return true;
-		} else return false;
-	}
+  publishEvent(customEvent) {
+    this.logSystem.debug(`Trying to publish event '${customEvent.id}`);
+    if (customEvent instanceof CustomEvent) {
+      PubSub.publish(customEvent, customEvent.id);
+      this.logSystem.info(`Published event '${customEvent.id}'`);
+      return true;
+    } else return false;
+  }
 
-	createEvent(guid, eventName, args = null) {
-		return new CustomEvent(guid, eventName, args);
-	}
+  createEvent(guid, eventName, args = null) {
+    this.logSystem.debug(
+      `Creating event '${eventName}' to instance id '${guid}' with args:${args} `
+    );
+    return new CustomEvent(guid, eventName, args);
+  }
 
-	// Actions methods
+  // Actions methods
 
-	createAction(actionName, guid, args = null) {
-		const instance = this.getSystem(guid);
-		// Warning!: nextline is very important. It's bind "this" of instance to callback
-		const callback = instance[actionName].bind(instance);
-		return new CustomAction(actionName, guid, callback, args);
-	}
+  createAction(actionName, guid, args = null) {
+    this.logSystem.debug(`Creating action:'${actionName}', guid:'${guid}', args:'${args}'`);
+    const instance = this.getInstance(guid);
+    // Warning!: nextline is very important. It's bind "this" of instance to callback
+    this.logSystem.debug(`Binding callback '${actionName}' to plugin instance`);
+    const callback = instance[actionName].bind(instance);
+    const action = new CustomAction(actionName, guid, callback, args);
+    this.logSystem.info(`Created action '${action.id}'`);
+    return action;
+  }
 
-	// Some API for comfortable action publishing
-	createActionByCallback(actionName, guid, callback, args = null) {
-		const customAction = new CustomAction(actionName, guid, callback, args);
-		this.actions.push(customAction);
-		return customAction;
-	}
+  // Some API for comfortable action publishing
+  createActionByCallback(actionName, guid, callback, args = null) {
+    this.logSystem.debug(
+      `Creating action:'${actionName}' by callback with 
+			guid:'${guid}', callback:'${callback.name}', args:'${args}'`
+    );
+    const customAction = new CustomAction(actionName, guid, callback, args);
+    this.actions.push(customAction);
+    this.logSystem.info(`Created action '${customAction.id}' by callback '${callback.name}'`);
+    return customAction;
+  }
 
-	// Subscribing
-	subscribeEventsByName(eventName, actionID) {
-		const events = this.findEventsByName(eventName);
-		const action = this.findActionById(actionID);
-		events.forEach(evt => this.subscribe(evt, action));
-		return true;
-	}
+  // Subscribing
+  subscribeEventsByName(eventName, actionID) {
+    this.logSystem.debug(`Subscribing eventName '${eventName}' to actionID '${actionID}'`);
+    const events = this.findEventsByName(eventName);
+    const action = this.findActionById(actionID);
+    if (events?.length != 0 && action) {
+      events.forEach(evt => {
+        this.subscribe(evt, action);
+        this.logSystem.info(`Subscribed event '${evt.id}' to action '${action.id}'`);
+      });
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	subscribeByNames(eventName, actionName) {
-		const events = this.findEventsByName(eventName);
-		const actions = this.findActionsByName(actionName);
-		for (let evt of events) {
-			for (let action of actions) {
-				this.subscribe(evt.id, action.id);
-			}
-		}
-		return true;
-	}
+  subscribeByNames(eventName, actionName) {
+    this.logSystem.debug(`Subscribing eventName '${eventName}' to actionName '${actionName}'`);
+    const events = this.findEventsByName(eventName);
+    const actions = this.findActionsByName(actionName);
+    for (let evt of events) {
+      for (let action of actions) {
+        this.subscribe(evt.id, action.id);
+        this.logSystem.info(`Subscribed event '${evt.id}' to action '${action.id}'`);
+      }
+    }
+    return true;
+  }
 
-	subscribe(eventID, actionID) {
-		const customAction = this.findActionById(actionID);
-		PubSub.subscribe(eventID, customAction.callback);
-		return true;
-	}
+  subscribe(eventID, actionID) {
+    this.logSystem.debug(`Subscribing event '${eventID}' to action '${actionID}'`);
+    const customAction = this.findActionById(actionID);
+    if (customAction) {
+      PubSub.subscribe(eventID, customAction.callback);
+      this.logSystem.info(`Subscribed event '${eventID}' to action '${customAction.id}'`);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-	subscribeEventNameByCallback(eventName, callback) {
-		const events = this.findEventsByName(eventName);
-		for (let evt of events) {
-			PubSub.subscribe(evt, callback);
-		}
-	}
+  subscribeEventNameByCallback(eventName, callback) {
+    this.logSystem.debug(`Subscribing event '${eventName}' to callback '${callback.name}'`);
+    const events = this.findEventsByName(eventName);
+    for (let evt of events) {
+      PubSub.subscribe(evt, callback);
+      this.logSystem.info(`Subscribed event '${evt.id}' to callback:${callback.name}`);
+    }
+  }
 
-	// Searching in actions/events
-	findActionById(actionID) {
-		return this.actions.find(action => action.id == actionID);
-	}
+  // Searching in actions/events
+  findActionById(actionID) {
+    this.logSystem.debug(`Finding action with the given actionID: '${actionID}'`);
+    const action = this.actions.find(action => action.id == actionID);
+    if (action) {
+      this.logSystem.debug(`Successfully found actions with id: '${actionID}'`);
+      return action;
+    } else {
+      this.logSystem.debug(`No action found with the given id: '${actionIDs}'`);
+      return;
+    }
+  }
 
-	findEventById(eventID) {
-		return this.events.find(evt => evt.id == eventID);
-	}
+  findEventById(eventID) {
+    this.logSystem.debug(`Finding event with the given eventID: '${eventID}'`);
+    const event = this.events.find(evt => evt.id == eventID);
+    if (event) {
+      this.logSystem.debug(`Successfully found event with id: '${eventID}'`);
+      return event;
+    } else {
+      this.logSystem.debug(`No event found with the given id: '${eventID}'`);
+      return;
+    }
+  }
 
-	findEventsByName(eventName) {
-		return this.events.filter(evt => evt.name == eventName);
-	}
+  findEventsByName(eventName) {
+    this.logSystem.debug(`Finding events with the given event name: '${eventName}'`);
+    const events = this.events.filter(evt => evt.name == eventName);
+    if (events?.length != 0) {
+      this.logSystem.debug(`Successfully found events with name: '${eventName}'`);
+      return events;
+    } else {
+      this.logSystem.debug(`No events found with the given name: '${eventName}'`);
+      return;
+    }
+  }
 
-	findActionsByName(actionName) {
-		return this.actions.filter(action => action.name == actionName);
-	}
+  findActionsByName(actionName) {
+    this.logSystem.debug(`Finding actions with the given event name: '${actionName}'`);
+    const actions = this.actions.filter(action => action.name == actionName);
+    if (actions?.length != 0) {
+      this.logSystem.debug(`Successfully found actions with name: '${actionName}'`);
+      return actions;
+    } else {
+      this.logSystem.debug(`No actions found with the given name: '${actionName}'`);
+      return;
+    }
+  }
 
-	showAvailableEvents() {
-		//TODO: prettify returned object (grouping etc)
-		return this.events;
-	}
+  showAvailableEvents() {
+    //TODO: prettify returned object (grouping etc)
+    return this.events;
+  }
 
-	showAvailableActions() {
-		//TODO: prettify returned object (grouping etc)
-		return this.actions;
-	}
+  showAvailableActions() {
+    //TODO: prettify returned object (grouping etc)
+    return this.actions;
+  }
 }
--- DTCD-EventSystem/tests/EventSystem.spec.js
+++ DTCD-EventSystem/tests/EventSystem.spec.js
@@ -1,119 +1,127 @@
-import {EventSystem} from '../src/EventSystem';
-import {CustomEvent} from '../src/utils/CustomEvent';
-import {CustomAction} from '../src/utils/CustomAction';
+import { EventSystem } from '../src/EventSystem';
+import { CustomEvent } from '../src/utils/CustomEvent';
+import { CustomAction } from '../src/utils/CustomAction';
 require('./pubsub');
 
 global.Application = {
-	getSystem: sysName => {
-		return {};
-	},
-	getSystem: guid => {
-		return {
-			testAction: () => {},
-		};
-	},
-	logSystem: {
-		logs: [],
-		log: function (logString) {
-			this.logs.push(logString);
-		},
-	},
+  getSystem: sysName => {
+    if (sysName == 'LogSystem') {
+      return {
+        info: () => {},
+        debug: () => {},
+        warn: () => {},
+        error: () => {},
+        fatal: () => {},
+      };
+    }
+  },
+  getInstance: guid => {
+    return {
+      testAction: () => {},
+    };
+  },
+  logSystem: {
+    logs: [],
+    log: function (logString) {
+      this.logs.push(logString);
+    },
+  },
 };
 
 let es = new EventSystem();
 
 describe('EventSystem:getRegistrationMeta', () => {
-	test('should be defined', () => {
-		expect(EventSystem.getRegistrationMeta).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(EventSystem.getRegistrationMeta).toBeDefined();
+  });
 });
 
 /* 
   Tests on create methods of EventSystem
 */
 describe('EventSystem:createEvent', () => {
-	test('should be defined', () => {
-		expect(es.createEvent).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.createEvent).toBeDefined();
+  });
 
-	test('returns expected event instance', () => {
-		const customEvent = new CustomEvent('guid1', 'testEvent');
-		expect(es.createEvent('guid1', 'testEvent')).toEqual(customEvent);
-	});
+  test('returns expected event instance', () => {
+    const customEvent = new CustomEvent('guid1', 'testEvent');
+    expect(es.createEvent('guid1', 'testEvent')).toEqual(customEvent);
+  });
 });
 
 describe('EventSystem:createAction', () => {
-	beforeEach(() => {
-		es = new EventSystem();
-	});
-	test('should be defined', () => {
-		expect(es.createAction).toBeDefined();
-	});
+  beforeEach(() => {
+    es = new EventSystem();
+  });
+  test('should be defined', () => {
+    expect(es.createAction).toBeDefined();
+  });
 
-	test('returns expected event instance', () => {
-		const action = es.createAction('testAction', 'guid1');
-		expect(action).toBeInstanceOf(CustomAction);
-		expect(es.registerAction(action)).toEqual(true);
-		expect(es.actions[0]).toEqual(action);
-	});
+  test('returns expected event instance', () => {
+    const action = es.createAction('testAction', 'guid1');
+    expect(action).toBeInstanceOf(CustomAction);
+    expect(es.registerAction(action)).toEqual(true);
+    expect(es.actions[0]).toEqual(action);
+  });
 });
 
 describe('EventSystem:createActionByCallback', () => {
-	beforeEach(() => {
-		es = new EventSystem();
-	});
-	test('should be defined', () => {
-		expect(es.createActionByCallback).toBeDefined();
-	});
+  beforeEach(() => {
+    es = new EventSystem();
+  });
+  test('should be defined', () => {
+    expect(es.createActionByCallback).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		const callback = () => {};
-		const customAction = new CustomAction('testAction', 'guid1', callback, null);
-		expect(es.createActionByCallback('testAction', 'guid1', callback)).toEqual(customAction);
-		expect(es.actions.length).toEqual(1);
-		expect(es.actions[0]).toEqual(customAction);
-	});
+  test('should return proper data', () => {
+    const callback = () => {};
+    const customAction = new CustomAction('testAction', 'guid1', callback, null);
+    expect(es.createActionByCallback('testAction', 'guid1', callback)).toEqual(customAction);
+    expect(es.actions.length).toEqual(1);
+    expect(es.actions[0]).toEqual(customAction);
+  });
 });
 /* 
   Tests on register methods of EventSystem
 */
 describe('EventSystem:registerEvent', () => {
-	beforeEach(() => {
-		es = new EventSystem();
-	});
-	test('should be defined', () => {
-		expect(es.registerEvent).toBeDefined();
-	});
+  beforeEach(() => {
+    es = new EventSystem();
+  });
+  test('should be defined', () => {
+    expect(es.registerEvent).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		const customEvent = new CustomEvent('guid1', 'testEvent');
-		expect(es.registerEvent(customEvent)).toEqual(true);
-		expect(es.events[0]).toEqual(customEvent);
-	});
-	test('should not accept not a CustomEvent instance', () => {
-		expect(es.registerEvent({})).toEqual(false);
-		expect(es.events.length).toEqual(0);
-	});
+  test('should return proper data', () => {
+    const customEvent = new CustomEvent('guid1', 'testEvent');
+    expect(es.registerEvent(customEvent)).toEqual(true);
+    expect(es.events[0]).toEqual(customEvent);
+  });
+  test('should not accept not a CustomEvent instance', () => {
+    expect(es.registerEvent({})).toEqual(false);
+    expect(es.events.length).toEqual(0);
+  });
 });
 
 describe('EventSystem:registerAction', () => {
-	beforeEach(() => {
-		es = new EventSystem();
-	});
-	test('should be defined', () => {
-		expect(es.registerAction).toBeDefined();
-	});
+  beforeEach(() => {
+    es = new EventSystem();
+  });
+  test('should be defined', () => {
+    expect(es.registerAction).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		const customAction = new CustomAction('testAction', 'guid1', () => console.log(1), null);
-		expect(es.registerAction(customAction)).toEqual(true);
-		expect(es.actions[0]).toEqual(customAction);
-	});
+  test('should return proper data', () => {
+    const customAction = new CustomAction('testAction', 'guid1', () => console.log(1), null);
+    expect(es.registerAction(customAction)).toEqual(true);
+    expect(es.actions[0]).toEqual(customAction);
+  });
 
-	test('should not accept not a CustomAction instance', () => {
-		expect(es.registerAction({})).toEqual(false);
-		expect(es.actions.length).toEqual(0);
-	});
+  test('should not accept not a CustomAction instance', () => {
+    expect(es.registerAction({})).toEqual(false);
+    expect(es.actions.length).toEqual(0);
+  });
 });
 
 /* 
@@ -121,39 +129,39 @@ describe('EventSystem:registerAction', () => {
 */
 
 describe('EventSystem:publishEvent', () => {
-	// const customEvent = new CustomEvent('guid1', 'testEvent');
-	// const callback = jest.fn().mockImplementationOnce((msg, data) => {
-	// 	console.log(msg, data);
-	// });
-	// it('pubsub', () => {
-	// 	PubSub.subscribe('MY TOPIC', console.log);
-	// 	console.log(PubSub.countSubscriptions());
-	// 	PubSub.publish('MY TOPIC', 'ITS ARGUMENT!');
-	// });
-	// it('FDSDSFDFSFDSFSDFDS', () => {
-	// 	expect(callback).toHaveBeenCalled();
-	// });
-	// beforeEach(() => {
-	// 	let es = new EventSystem();
-	// 	es.logSystem = Application.logSystem;
-	// });
-	// test('should be defined', () => {
-	// 	expect(es.publishEvent).toBeDefined();
-	// });
-	// test('should return proper data', () => {
-	// 	expect(es.registerEvent(customEvent)).toEqual(true);
-	// es.subscribeEventNameByCallback('testEvent', callback);
-	// PubSub.publish('MY TOPIC', 'hello world!');
-	// const customAction = new CustomAction('testAction', 'guid1', callback, null);
-	// expect(es.registerAction(customAction)).toEqual(true);
-	// PubSub.subscribe(customEvent.id, callback);
-	// es.createAndPublish('guid1', 'testEvent');
-	// console.log(PubSub.getSubscriptions());
-	// expect(callback).toHaveBeenCalled();
-	// });
-	// test('should not accept not a CustomEvent instance', () => {
-	// 	expect(es.publishEvent({})).toEqual(false);
-	// });
+  // const customEvent = new CustomEvent('guid1', 'testEvent');
+  // const callback = jest.fn().mockImplementationOnce((msg, data) => {
+  // 	console.log(msg, data);
+  // });
+  // it('pubsub', () => {
+  // 	PubSub.subscribe('MY TOPIC', console.log);
+  // 	console.log(PubSub.countSubscriptions());
+  // 	PubSub.publish('MY TOPIC', 'ITS ARGUMENT!');
+  // });
+  // it('FDSDSFDFSFDSFSDFDS', () => {
+  // 	expect(callback).toHaveBeenCalled();
+  // });
+  // beforeEach(() => {
+  // 	let es = new EventSystem();
+  // 	es.logSystem = Application.logSystem;
+  // });
+  // test('should be defined', () => {
+  // 	expect(es.publishEvent).toBeDefined();
+  // });
+  // test('should return proper data', () => {
+  // 	expect(es.registerEvent(customEvent)).toEqual(true);
+  // es.subscribeEventNameByCallback('testEvent', callback);
+  // PubSub.publish('MY TOPIC', 'hello world!');
+  // const customAction = new CustomAction('testAction', 'guid1', callback, null);
+  // expect(es.registerAction(customAction)).toEqual(true);
+  // PubSub.subscribe(customEvent.id, callback);
+  // es.createAndPublish('guid1', 'testEvent');
+  // console.log(PubSub.getSubscriptions());
+  // expect(callback).toHaveBeenCalled();
+  // });
+  // test('should not accept not a CustomEvent instance', () => {
+  // 	expect(es.publishEvent({})).toEqual(false);
+  // });
 });
 
 /* 
@@ -161,68 +169,68 @@ describe('EventSystem:publishEvent', () => {
 */
 
 describe('EventSystem:findActionById', () => {
-	beforeEach(() => {
-		es = new EventSystem();
-	});
-	test('should be defined', () => {
-		expect(es.findActionById).toBeDefined();
-	});
+  beforeEach(() => {
+    es = new EventSystem();
+  });
+  test('should be defined', () => {
+    expect(es.findActionById).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		const customAction = new CustomAction('testAction', 'guid1', () => console.log(1), null);
-		expect(es.registerAction(customAction)).toEqual(true);
-		expect(es.findActionById('guid1[testAction]')).toEqual(customAction);
-	});
+  test('should return proper data', () => {
+    const customAction = new CustomAction('testAction', 'guid1', () => console.log(1), null);
+    expect(es.registerAction(customAction)).toEqual(true);
+    expect(es.findActionById('guid1[testAction]')).toEqual(customAction);
+  });
 });
 
 describe('EventSystem:findEventById', () => {
-	test('should be defined', () => {
-		expect(es.findEventById).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.findEventById).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		const customEvent = new CustomEvent('guid1', 'testEvent');
-		expect(es.registerEvent(customEvent)).toEqual(true);
-		expect(es.findEventById('testEvent[guid1]')).toEqual(customEvent);
-	});
+  test('should return proper data', () => {
+    const customEvent = new CustomEvent('guid1', 'testEvent');
+    expect(es.registerEvent(customEvent)).toEqual(true);
+    expect(es.findEventById('testEvent[guid1]')).toEqual(customEvent);
+  });
 });
 
 describe('EventSystem:findEventsByName', () => {
-	test('should be defined', () => {
-		expect(es.findEventsByName).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.findEventsByName).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		expect(es.findEventsByName('testEvent')).toEqual(expect.any(Array));
-	});
+  test('should return proper data', () => {
+    expect(es.findEventsByName('testEvent')).toEqual(expect.any(Array));
+  });
 });
 
 describe('EventSystem:findActionsByName', () => {
-	test('should be defined', () => {
-		expect(es.findActionsByName).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.findActionsByName).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		expect(es.findActionsByName('testAction')).toEqual(expect.any(Array));
-	});
+  test('should return proper data', () => {
+    expect(es.findActionsByName('testAction')).toEqual(expect.any(Array));
+  });
 });
 
 describe('EventSystem:showAvailableEvents', () => {
-	test('should be defined', () => {
-		expect(es.showAvailableEvents).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.showAvailableEvents).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		expect(es.showAvailableEvents()).toEqual(expect.any(Array));
-	});
+  test('should return proper data', () => {
+    expect(es.showAvailableEvents()).toEqual(expect.any(Array));
+  });
 });
 
 describe('EventSystem:showAvailableActions', () => {
-	test('should be defined', () => {
-		expect(es.showAvailableActions).toBeDefined();
-	});
+  test('should be defined', () => {
+    expect(es.showAvailableActions).toBeDefined();
+  });
 
-	test('should return proper data', () => {
-		expect(es.showAvailableActions()).toEqual(expect.any(Array));
-	});
+  test('should return proper data', () => {
+    expect(es.showAvailableActions()).toEqual(expect.any(Array));
+  });
 });
