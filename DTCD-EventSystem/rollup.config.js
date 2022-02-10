52b1f952aeb6887a7687ca533bdb824b12716d48
konstantin-isg <konstantin@isgneuro.com>
2022-02-10 14:58:22 +0300

    Added private properties and methods processing by babel

--- DTCD-EventSystem/rollup.config.js
+++ DTCD-EventSystem/rollup.config.js
@@ -1,3 +1,5 @@
+import babel from '@rollup/plugin-babel';
+
 const watch = Boolean(process.env.ROLLUP_WATCH);
 
 const pluginName = 'EventSystem';
@@ -6,7 +8,9 @@ const fileDest = watch
   ? `./../../DTCD/server/plugins/DTCD-${pluginName}/${pluginName}.js`
   : `./build/${pluginName}.js`;
 
-const plugins = [];
+const plugins = [
+  babel({ babelHelpers: 'bundled' }),
+];
 
 export default {
   input: './src/EventSystem.js',
